9af8a2a55705fcaa3567f7bd2f0de41ec1ef7c81
Ivan Steblenko <akudm2007@yandex.ru>
2016-04-13 14:52:47 +0400

    add services

--- app/components/pages/services-page/services.page.component.js
+++ app/components/pages/services-page/services.page.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', '../../../services/services.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,22 +10,53 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, services_service_1;
     var ServicesPageComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (services_service_1_1) {
+                services_service_1 = services_service_1_1;
             }],
         execute: function() {
             ServicesPageComponent = (function () {
-                function ServicesPageComponent() {
+                function ServicesPageComponent(_services) {
+                    this._services = _services;
+                    this._rows = [];
                 }
+                ServicesPageComponent.prototype.ngOnInit = function () {
+                    this._services.getListOfServices().then(function (data) {
+                        this._servicesList = data;
+                        this.prepareRows();
+                    }.bind(this));
+                };
+                ServicesPageComponent.prototype.prepareRows = function () {
+                    var count = Math.floor(this._servicesList.length / 3);
+                    var reminder = this._servicesList.length % 3;
+                    if (count && count > 0) {
+                        for (var index = 0; index < count; index++) {
+                            var row = [];
+                            for (var j = 0; j < 3; j++) {
+                                row.push(this._servicesList[index * 3 + j]);
+                            }
+                            this._rows.push(row);
+                        }
+                    }
+                    if (reminder && reminder > 0) {
+                        var row = [];
+                        for (var index = reminder - 1; index >= 0; index--) {
+                            row.push(this._servicesList[this._servicesList.length - index]);
+                        }
+                        this._rows.push(row);
+                    }
+                };
                 ServicesPageComponent = __decorate([
                     core_1.Component({
                         templateUrl: '../app/templates/services.page.component.html'
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [services_service_1.ServicesService])
                 ], ServicesPageComponent);
                 return ServicesPageComponent;
             }());
--- app/components/pages/services-page/services.page.component.ts
+++ app/components/pages/services-page/services.page.component.ts
@@ -1,7 +1,42 @@
-import { Component} from 'angular2/core';
+import { Component, OnInit} from 'angular2/core';
+import { ServicesService } from '../../../services/services.service';
+import { Service } from '../../../data-interfaces/service';
 
 @Component({
     templateUrl: '../app/templates/services.page.component.html'
 })
-export class ServicesPageComponent {
+export class ServicesPageComponent implements OnInit {
+    private _servicesList: Service[];
+    private _rows = [];
+    
+    constructor(private _services: ServicesService) {
+    }
+    
+    ngOnInit() {
+        this._services.getListOfServices().then(function (data) {
+            this._servicesList = data;
+            this.prepareRows();
+         }.bind(this))   
+    }
+    
+    prepareRows() {
+        let count = Math.floor(this._servicesList.length/3);
+        let reminder = this._servicesList.length%3;
+        if (count && count > 0) {                 
+            for (let index = 0; index < count; index++) {
+                let row = [];
+                for(let j = 0; j < 3; j++) {
+                    row.push(this._servicesList[index*3 + j]);
+                }
+                this._rows.push(row);
+            }
+        }
+        if (reminder && reminder > 0) {
+            let row = [];
+            for (let index = reminder-1; index >= 0; index--) {
+                row.push(this._servicesList[this._servicesList.length-index]);
+            }
+            this._rows.push(row);
+        }
+    }
 }
