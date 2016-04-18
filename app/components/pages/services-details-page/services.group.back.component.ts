4f48212ad0555d464be9c7c245c0d820fe9e3eab
Ivan Steblenko <akudm2007@yandex.ru>
2016-04-18 14:00:31 +0400

    fix styles

--- app/components/pages/services-details-page/services.group.back.component.ts
+++ app/components/pages/services-details-page/services.group.back.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from 'angular2/core';
+import { ROUTER_DIRECTIVES } from 'angular2/router';
 
 @Component({
     selector: 'services-group-back-component',
-    templateUrl: '../app/templates/services.group.back.component.html'
+    templateUrl: '../app/templates/services.group.back.component.html',
+    directives: [ROUTER_DIRECTIVES]
 })
 export class ServicesGroupBackComponent implements OnInit {
     constructor() { }
