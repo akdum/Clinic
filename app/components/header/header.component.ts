dde9e39c88752edb44ebb99c8a7ae7afca70e2d7
Ivan Steblenko <akudm2007@yandex.ru>
2016-04-29 17:22:46 +0400

    remove carousel

--- app/components/header/header.component.ts
+++ app/components/header/header.component.ts
@@ -1,7 +1,6 @@
 import {Component} from 'angular2/core';
 import { LogoComponent } from './logo.component';
 import { ContactsComponent } from './contacts.component';
-import { CarouselComponent } from './carousel.component';
 import { MobileNavComponent } from './mobile.nav.component';
 import { SloganComponent } from './slogan.component';
 
