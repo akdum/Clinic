fb80a62060399aa0d0de7a4ec0359ef651eefd42
Ivan Steblenko <akudm2007@yandex.ru>
2016-05-31 11:55:16 +0400

    small fixies

--- app/components/header/slogan.component.ts
+++ app/components/header/slogan.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 
 @Component({
     selector: 'slogan-component',
-    templateUrl: '../app/templates/slogan.component.html'
+    templateUrl: '../app/templates/slogan.component.html',
+    directives: [ROUTER_DIRECTIVES]
 })
 export class SloganComponent {
 }
